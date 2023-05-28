8cee778e4fa46cd01b353af451b253ec0746bf17
smhty <hussein.ty@gmail.com>
2023-05-28 10:33:07 -0700

    folder

---
+++ dorna_2s/upgrade.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+pip3 install -r requirements.txt
+python3 ENC.py
+python3 Unlock_Flash.py
+python3 Firmware_Upgrade.py
+python3 Lock_Flash.py
+rm STM32_Bootloader_Host.py
+rm firmware.bin
+rm Unlock_Flash.py
+rm Firmware_Upgrade.py
+rm Lock_Flash.py
+rm ENC.py
+rm log.txt
+rm efirmware
+rm upgrade.sh
